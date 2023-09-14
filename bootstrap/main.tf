2bcead8abea2b33274e78c94bbf9cc697fb4b8c3
shriramgaddam <shriram.gaddam@springml.com>
2023-09-13 21:19:07 -0600

    removing app trigger

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -65,36 +65,36 @@ resource "google_cloudbuildv2_repository" "my_repository" {
   }
 
 
-# resource "google_cloudbuild_trigger" "infra-trigger" {
-#   location = "us-central1"
-#   project = "presales-team-mahesh-15jul"
-#   name    =  "dev-infra-trigger"
-#   ignored_files      = ["gke_terraform/application/**"]
-
-#   repository_event_config {
-#     repository = google_cloudbuildv2_repository.my_repository.id
-#     push {
-#       branch = "dev"
-#     }
-#   }
-
-#   filename = "cloudbuild.yaml"
-# }
-
-
-resource "google_cloudbuild_trigger" "application-trigger" {
+resource "google_cloudbuild_trigger" "infra-trigger" {
   location = "us-central1"
   project = "presales-team-mahesh-15jul"
-  name    =  "dev-application-branch"
-  included_files      = ["application/**"]
-  
+  name    =  "dev-infra-trigger"
+  ignored_files      = ["gke_terraform/application/**"]
 
   repository_event_config {
     repository = google_cloudbuildv2_repository.my_repository.id
     push {
-      branch = "dev-application"
+      branch = "dev"
     }
   }
 
-  filename = "cloudbuild-app.yaml"
+  filename = "cloudbuild.yaml"
 }
+
+
+# resource "google_cloudbuild_trigger" "application-trigger" {
+#   location = "us-central1"
+#   project = "presales-team-mahesh-15jul"
+#   name    =  "dev-application-branch"
+#   included_files      = ["application/**"]
+  
+
+#   repository_event_config {
+#     repository = google_cloudbuildv2_repository.my_repository.id
+#     push {
+#       branch = "dev-application"
+#     }
+#   }
+
+#   filename = "cloudbuild-app.yaml"
+# }
