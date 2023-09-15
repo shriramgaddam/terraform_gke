828cea691612c247d7f17205727a088414538dc4
shriramgaddam <shriram.gaddam@springml.com>
2023-09-15 09:58:37 -0600

    removed old cb dir

--- environments/cloudbuild-resources/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  project                     = var.project_id
-  request_timeout             = var.provider_request_timeout
-  region                      = var.default_region
-  #impersonate_service_account = var.terraform_service_account
-}
