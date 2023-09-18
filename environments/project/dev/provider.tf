e9c3f71d0f9febd071d7fd4520ccb3d446f8e58b
shriramgaddam <shriram.gaddam@springml.com>
2023-09-18 00:07:52 -0600

    dfsdg

--- environments/project/dev/provider.tf
+++ environments/project/dev/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project                     = var.project_id
-  request_timeout             = var.provider_request_timeout
+  #request_timeout             = var.provider_request_timeout
   region                      = var.default_region 
 }
 
