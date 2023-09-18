40b59a922200df0aea1ff7c122a55a1974e6ea9c
shriramgaddam <shriram.gaddam@springml.com>
2023-09-18 08:11:24 -0600

    cloudbuild file infra trigger test2

--- environments/resources/dev/provider.tf
+++ environments/resources/dev/provider.tf
@@ -23,7 +23,7 @@ terraform {
 provider "google" {
   project = var.project_id
   #impersonate_service_account = var.tf_impersonate_service_account
-  #request_timeout = var.provider_request_timeout
+  request_timeout = var.provider_request_timeout
   region          = var.default_region
 }
 
