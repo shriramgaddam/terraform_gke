453cba02de645d50fa4e0b5c210189a4dbbe32d2
shriramgaddam <shriram.gaddam@springml.com>
2023-09-19 23:36:11 -0600

    www

--- environments/resources/dev/provider.tf
+++ environments/resources/dev/provider.tf
@@ -38,39 +38,39 @@ data "google_client_config" "default" {
 }
 
 
-# data "google_container_cluster" "default" {
-#   # as-is this will probably need to be run a few times
-#   name = "${var.project_id}-gke"
-#   # TODO: make this dependency?
-#   location = var.default_region
-#   project  = var.project_id
-# }
+data "google_container_cluster" "default" {
+  # as-is this will probably need to be run a few times
+  name = "${var.project_id}-gke"
+  # TODO: make this dependency?
+  location = var.default_region
+  project  = var.project_id
+}
 
-# provider "kubernetes" {
-#   host  = "https://${data.google_container_cluster.default.endpoint}"
-#   token = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(
-#     data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-#   )
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.default.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
 
-# }
+}
 
-# provider "kubectl" {
-#   load_config_file = false
-#   host             = "https://${data.google_container_cluster.default.endpoint}"
-#   token            = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(
-#     data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-#   )
-# }
+provider "kubectl" {
+  load_config_file = false
+  host             = "https://${data.google_container_cluster.default.endpoint}"
+  token            = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host  = "https://${data.google_container_cluster.default.endpoint}"
-#     token = data.google_client_config.default.access_token
-#     cluster_ca_certificate = base64decode(
-#       data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-#     )
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host  = "https://${data.google_container_cluster.default.endpoint}"
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+    )
+  }
+}
 
